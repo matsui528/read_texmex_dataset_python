4a441f053ced280905ccfdf64890964d01297de7
Hi-king <hikingko1@gmail.com>
2016-10-13 23:53:21 +0900

    :star: multiprocessing read

--- reader.py
+++ reader.py
@@ -1,24 +1,39 @@
 import struct
 import numpy
+import os
 
-def read_base(filename, typechar, typesize):
+def read_base(filename, typechar, typesize, skipsample=0):
     with open(filename, 'rb') as f:
+        if skipsample > 0:
+            d_bin = f.read(4)
+            dim, = struct.unpack('i', d_bin)
+            f.seek(dim*typesize*skipsample + 4*skipsample, 0)
         while True:
             vec = []
             d_bin = f.read(4)
-            if d_bin=='':
+            if d_bin==b'':
                 break
             dim, = struct.unpack('i', d_bin)
+            # f.read(dim*typesize)
             for d in range(dim):
-                value, = struct.unpack(typechar, f.read(typesize))
-                vec.append(value)
+               value, = struct.unpack(typechar, f.read(typesize))
+               vec.append(value)
             yield vec
+
+def get_sample_size(filename, typechar, typesize):
+    with open(filename, 'rb') as f:
+        d_bin = f.read(4)
+        dim, = struct.unpack('i', d_bin)
+        return os.path.getsize(filename) / dim /typesize
+
 
 #float
 def read_fvec(filename):
     return numpy.array(list(read_fvec_iter(filename)))
-def read_fvec_iter(filename):
-    return read_base(filename, 'f', 4)
+def read_fvec_iter(filename, skipsample=0):
+    return read_base(filename, 'f', 4, skipsample=skipsample)
+def get_fvec_size(filename):
+    return get_sample_size(filename, 'f', 4)
 
 #unsigned char
 def read_bvec(filename):
--- save_fvec.py
+++ save_fvec.py
@@ -3,13 +3,38 @@ import struct
 import numpy
 import reader
 import csv
+import multiprocessing
 
 parser = argparse.ArgumentParser()
 parser.add_argument("filepath")
 parser.add_argument("savepath")
+parser.add_argument("--split", type=int)
 args = parser.parse_args()
 
-with open(args.savepath, "w+") as fw:
-    writer = csv.writer(fw)
-    for vec in reader.read_fvec_iter(args.filepath):
-        writer.writerow(vec)
+def process(input_path, output_path, offset, count):
+    with open(output_path, "w+") as fw:
+        writer = csv.writer(fw)
+        for i,vec in enumerate(reader.read_fvec_iter(args.filepath, skipsample=offset)):
+            if i % 100000 == 0:
+                print(i)
+            if i == count:
+                break
+            writer.writerow(vec)
+
+if __name__ == '__main__':
+    if args.split is None:
+        args.split = reader.get_fvec_size(args.filepath)
+        print(args.split)
+    processes = []
+    for i in xrange(0, reader.get_fvec_size(args.filepath), args.split):
+        p = multiprocessing.Process(target=process, args=(
+            args.filepath, 
+            "{}_{}".format(args.savepath, i),
+            i,
+            args.split
+        ))
+        processes.append(p)
+        p.start()
+        print("started")
+    for process in processes:
+        process.join()
