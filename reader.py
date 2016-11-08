67285dc16f463837ab57930287a71f9f12718e4f
Hi-king <hikingko1@gmail.com>
2016-11-08 23:22:48 +0900

    :lipstick: format

--- reader.py
+++ reader.py
@@ -44,7 +44,7 @@ def get_sample_size(filename, typechar, typesize):
     with open(filename, 'rb') as f:
         d_bin = f.read(4)
         dim, = struct.unpack('i', d_bin)
-        return os.path.getsize(filename) / dim /typesize
+        return int(os.path.getsize(filename) / dim /typesize)
 
 #float
 def read_fvec(filename):
@@ -57,5 +57,7 @@ def get_fvec_size(filename):
 #unsigned char
 def read_bvec(filename):
     return numpy.array(list(read_bvec_iter(filename)))
-def read_bvec_iter(filename):
-    return read_base(filename, 'B', 1)
+def read_bvec_iter(filename, skipsample=0):
+    return read_base(filename, 'B', 1, skipsample=skipsample)
+def get_bvec_size(filename):
+    return get_sample_size(filename, 'B', 1)
---
+++ save_bvec_multiprocess.py
@@ -0,0 +1,41 @@
+from builtins import range
+import argparse
+import struct
+import numpy
+import reader
+import csv
+import multiprocessing
+
+parser = argparse.ArgumentParser()
+parser.add_argument("filepath")
+parser.add_argument("savepath")
+parser.add_argument("--split", type=int, required=True)
+args = parser.parse_args()
+
+def process(input_path, output_path, offset, count):
+    with open(output_path, "w+") as fw:
+        writer = csv.writer(fw)
+        for i,vec in enumerate(reader.read_bvec_iter(args.filepath, skipsample=offset)):
+            if i % 100000 == 0:
+                print(i)
+            if i == count:
+                break
+            writer.writerow(vec)
+
+if __name__ == '__main__':
+    num_samples = reader.get_bvec_size(args.filepath)
+    if args.split is None:
+        args.split = num_samples
+    processes = []
+    for i in range(0, num_samples, args.split):
+        p = multiprocessing.Process(target=process, args=(
+            args.filepath, 
+            ("{}_{:0"+str(len(str(num_samples)))+"d}").format(args.savepath, i),
+            i,
+            args.split
+        ))
+        processes.append(p)
+        p.start()
+        print("started")
+    for process in processes:
+        process.join()
